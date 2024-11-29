dc9f3ba50f8dd41aef42c68de27170ca616e4ffb
Niric02 <cyrill@tschopp.eu>
2024-11-29 09:59:18 +0100

    Not working! need to get changes from laptop to desktop and dont want to bother with patches

--- src/llm/ReplicatePrompter.py
+++ src/llm/ReplicatePrompter.py
@@ -2,10 +2,6 @@ import replicate
 
 class ReplicatePrompter:
 
-    prompt_template = ("<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n\n"
-                       "Your job is to take input from a speech to text model and create a prompt for a image generation model. Only Provide the prompt, no niceties required, make it so it is in an anime style. And feel free to be colorful in discrybing the scene"
-                       " <|eot_id|><|start_header_id|>user<|end_header_id|>\n\n{prompt}<|eot_id|><|start_header_id|>prompter<|end_header_id|>\n\n")
-
     def __init__(self,client,logger):
         self.client = client
         self.logger = logger
@@ -15,17 +11,17 @@ class ReplicatePrompter:
                             f"{context}"
                             f" <|eot_id|><|start_header_id|>user<|end_header_id|>\n\n{{prompt}}<|eot_id|><|start_header_id|>prompter<|end_header_id|>\n\n")
 
-    def create_input(self,prompt):
+    def create_input(self,prompt,prompt_template):
         return {
                 "top_p": 0.9,
                 "prompt": prompt,
                 "min_tokens": 0,
                 "temperature": 0.6,
-                "prompt_template": self.prompt_template,
+                "prompt_template": prompt_template,
                 "presence_penalty": 1.15
                 }
 
-    def run(self, prompt):
+    def run(self, prompt,context):
         output = ""
 
         for event in self.client.stream(
--- src/speech_to_picture/ImageFromSpeech.py
+++ src/speech_to_picture/ImageFromSpeech.py
@@ -11,15 +11,16 @@ class ImageFromSpeech:
         self.logger = logger
         self.replicate_client = replicate_client
         self.llm = ReplicatePrompter(self.replicate_client, logger)
-        self.llm.set_context(os.getenv('CONTEXT'))
-
         self.imager = ReplicateImageModel(self.replicate_client, logger)
+        self.prompt_history = []
 
     def generate_image_from_transcription(self, transcript: str):
         self.append_transcription_to_file(transcript)
+        self.prompt_history.append({"role": "user", "content": transcript})
         self.logger.info(transcript)
         prompt = self.llm.run(transcript)
         self.append_prompt_to_file(prompt)
+        self.prompt_history.append({"role": "prompter", "content": prompt})
         image_path = self.imager.run(prompt)
         return image_path
 
@@ -58,3 +59,13 @@ class ImageFromSpeech:
         with open(file_path, "a") as file:
             # Write the transcription to the file followed by a newline
             file.write(f"prompt: [{prompt}]\n\n")
+
+    def generate_prompt_template(self):
+        context = os.getenv('CONTEXT')
+
+        for history in self.prompt_history:
+
+
+        prompt_template = (f"<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n\n"
+                            f"{context}"
+                            f" <|eot_id|><|start_header_id|>user<|end_header_id|>\n\n{{prompt}}<|eot_id|><|start_header_id|>prompter<|end_header_id|>\n\n")
