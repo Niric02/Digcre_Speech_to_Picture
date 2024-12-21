167384a63e6f8a1d972184fdf322f14baa79ab24
Niric02 <cyrill@tschopp.eu>
2024-12-21 11:29:30 +0100

    exchanged the flux pro ultra model with flux dev to disable nsfw filter

--- src/image_model/ReplicateImageModel.py
+++ src/image_model/ReplicateImageModel.py
@@ -10,21 +10,36 @@ class ReplicateImageModel:
         self.logger = logger.getChild('image')
 
     def create_imput(self, prompt):
-        input = {"raw": False,
-                 "prompt": prompt,
-                 "aspect_ratio": "3:2",
-                 "output_format": "jpg",
-                 "safety_tolerance": 2,
-                 "image_prompt_strength": 0.1,
-                 "seed": 1234
-                 }
+        #input = {"raw": False,
+        #         "prompt": prompt,
+        #         "aspect_ratio": "3:2",
+        #         "output_format": "jpg",
+        #         "safety_tolerance": 6,
+        #         "image_prompt_strength": 0.1,
+        #         "seed": 6969693245266
+        #         }
+
+        input = {
+            "prompt": prompt,
+            "go_fast": True,
+            "guidance": 3.5,
+            "num_outputs": 1,
+            "aspect_ratio": "3:2",
+            "output_format": "jpg",
+            "output_quality": 80,
+            "prompt_strength": 0.8,
+            "num_inference_steps": 28,
+            "seed": 6969693245266,
+            "disable_safety_checker": True
+        }
         return input
 
     async def run(self, prompt):
         input = self.create_imput(prompt)
         self.logger.info(f"generating Image from prompt: {input}")
         output = await self.client.async_run(
-            "black-forest-labs/flux-1.1-pro-ultra",
+            #"black-forest-labs/flux-1.1-pro-ultra",
+            "black-forest-labs/flux-dev",
             input=input,
         )
 
@@ -40,9 +55,11 @@ class ReplicateImageModel:
         # Construct the full path to save the image
         file_path = os.path.join(images_dir, filename)
 
+        print(output)
+
         # Write the image to the file
         with open(file_path, "wb") as file:
-            file.write(output.read())
+            file.write(output[0].read())
 
         self.logger.info(f"Image saved to {file_path}")
 
