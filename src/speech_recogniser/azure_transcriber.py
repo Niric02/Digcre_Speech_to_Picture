94c9b505944cb72c19e82964c3a26268f1c6a658
Alexander Wyss <alexsilvan.wyss@gmail.com>
2025-01-04 14:32:17 +0100

    delete unused files

--- src/speech_recogniser/azure_transcriber.py
+++ src/speech_recogniser/azure_transcriber.py
@@ -1,28 +1,6 @@
 import azure.cognitiveservices.speech as speechsdk
 
 
-#class Transcriber:
-#
-#    def __init__(self,key,region):
-#        self.speech_config = speechsdk.SpeechConfig(subscription=key, region=region)
-#
-#    def from_file(self, filename):
-#
-#        audio_config = speechsdk.AudioConfig(filename=filename)
-#        speech_recognizer = speechsdk.SpeechRecognizer(speech_config=self.speech_config, audio_config=audio_config)
-#        speech_recognizer.recognized.connect(self.recognize_speech)
-#
-#        speech_recognition_result = speech_recognizer.start_continuous_recognition_async()
-#        return speech_recognition_result.text
-#
-#    def recognize_speech(self,evt):
-#        print("is recognising speech")
-#
-#    def speech_end_detected(self,evt):
-#        print("speech_end_detected")
-#        evt.stop_keyword_recognition_async()
-
-
 class Transcriber:
     def __init__(self, key, region):
         self.speech_config = speechsdk.SpeechConfig(subscription=key, region=region)
